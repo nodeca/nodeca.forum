57a447277435a0574b24db49c1a3a9912ee990fd
shkuropat <e.shkuropat@gmail.com>
2012-03-26 15:19:39 +0400

    Added app initialization file

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+// nodeca
+var NLib = require('nlib');
+
+module.exports = NLib.Application.create({
+  root: __dirname,
+  name: 'nodeca.forum',
+  bootstrap: function (nodeca, callback) {
+    // empty bootstrap... for now..
+    callback();
+  }
+});
