62c694319559bca195d1c3f7edfb37e0f434b52a
shkuropat <e.shkuropat@gmail.com>
2012-07-02 13:04:09 +0400

    Refactored forum three building

--- server/forum/index.js
+++ server/forum/index.js
@@ -47,47 +47,11 @@ function fetch_sections(params, next) {
 module.exports = function (params, next) {
 
   var user_id_list = this.data.users = [];
-  var sections = _.values(nodeca.cache.get('sections', [])).map(function(section) {
-    // ToDo check permissions
-    var doc = section._doc;
-    doc._id = doc._id.toString();
-    if (doc.parent) {
-      doc.parent = doc.parent.toString();
-    }
-    else {
-      doc.parent = null;
-    }
-    var moderators = doc.moderator_list.map(function(user) {
-      user_id_list.push( user.toString());
-      return user.toString();
-    });
+  var sections = nodeca.cache.get('sections', []);
 
-    if (doc.cache.real.last_user) {
-      user_id_list.push(doc.cache.real.last_user.toString());
-    }
-
-    // ToDo replace real for hb users
-    return {
-      _id:              doc._id,
-      id:               doc.id,
-      title:            doc.title,
-      description:      doc.description,
-      parent:           doc.parent,
-      redirect:         doc.redirect,
-      moderators:       moderators,
-      thread_count:     doc.cache.real.thread_count,
-      post_count:       doc.cache.real.post_count,
-      display_order:    doc.display_order,
-      last_thread: {
-        title:          doc.cache.real.last_thread_title,
-        id:             doc.cache.real.last_thread_id,
-        post_id:        doc.cache.real.last_post_id,
-        user:           doc.cache.real.last_user,
-        ts:             doc.cache.real.last_ts
-      }
-    };
+  this.response.data.sections = build_tree(sections, null, 3, function(section) {
+    return prepare_section_display_info(section, user_id_list);
   });
-  this.response.data.sections = build_tree(sections);
   next();
 };
 
