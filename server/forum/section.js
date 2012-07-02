73aabc228c69ec6d1ec5430f5c960239184c1648
shkuropat <e.shkuropat@gmail.com>
2012-07-02 13:04:32 +0400

    Added sub-forums on sections page

--- server/forum/section.js
+++ server/forum/section.js
@@ -7,13 +7,27 @@ var Thread = nodeca.models.forum.Thread;
 
 var forum_breadcrumbs = require('../../lib/widgets/breadcrumbs.js').forum;
 
+var forum_helpers = require('../../lib/helpers/forum.js');
+var build_tree = forum_helpers.build_tree;
+var prepare_section_display_info = forum_helpers.prepare_section_display_info;
 
-// fetch and prepare threads
+
+// fetch and prepare threads and sub-forums
 // ToDo add sorting and pagination
 module.exports = function (params, next) {
   var data = this.response.data;
 
   var user_id_list = this.data.users = [];
+
+  // prepare sub-forums
+  var sections = nodeca.cache.get('sections', []);
+  var root = sections[params.id]._id;
+
+  this.response.data.sections = build_tree(sections, root, 2, function(section) {
+    return prepare_section_display_info(section, user_id_list);
+  });
+
+  // fetch and prepare threads
   Thread.fetchThredsByForumId(params.id, function (err, threads) {
     data.threads = threads.map(function(thread) {
       // ToDo check permissions
