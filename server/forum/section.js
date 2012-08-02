b4ad29c7fa1dc5319fb4e9631c71a6d746999f9a
Vitaly Puzrin <vitaly@rcdesign.ru>
2012-08-02 13:52:47 +0400

    temporary fix for variable type casting, until validator available

--- server/forum/section.js
+++ server/forum/section.js
@@ -326,7 +326,7 @@ nodeca.filters.after('@', function (params, next) {
   threads_per_page = nodeca.settings.global.get('threads_per_page');
   data.page = {
     max: Math.ceil(forum.cache.real.thread_count / threads_per_page),
-    current: params.page,
+    current: ~~params.page,
   };
 
   // fetch breadcrumbs data
