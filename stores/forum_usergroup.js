b4866aa0137731b7b8ac1d63c380745ad6c6cc14
Aleksey V Zapparov <ixti@member.fsf.org>
2012-10-25 13:01:04 +0200

    Implement forum user group settings store

---
+++ stores/forum_usergroup.js
@@ -0,0 +1,104 @@
+'use strict';
+
+
+/*global nodeca, _*/
+
+
+// 3rd-party
+var Store = require('nlib').Settings.Store;
+var async = require('nlib').Vendor.Async;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Helper to fetch usergroups by IDs
+//
+function fetchForumSettings(id, callback) {
+  nodeca.models.users.Forum.find()
+    .select('settings')
+    .where('_id').in(id)
+    .exec(callback);
+}
+
+
+// Memoized version of fetchUserGroups helper
+//
+var fetchForumSesstingsCached = nodeca.components.memoizee(fetchForumSettings, {
+  // momoizee options. revalidate cache after 30 sec
+  async:  true,
+  maxAge: 30000
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+var ForumUserGroupStore = new Store({
+  get: function (key, params, options, callback) {
+    var func = options.skipCache ? fetchForumSettings : fetchForumSesstingsCached;
+
+    func(params.forum_id, function (err, forum) {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      var settings  = forum.settings || {};
+      var values    = [];
+
+      params.usergroup_ids.forEach(function (id) {
+        if (settings[key + ':usergroup:' + id]) {
+          values.push(settings[key + ':usergroup:' + id]);
+        }
+      });
+
+      // push default value
+      values.push({ value: ForumUserGroupStore.getDefaultValue(key) });
+
+      callback(null, Store.mergeValues(values));
+    });
+  },
+  set: function (values, params, callback) {
+    fetchForumSettings(params.forum_id, function (err, forum) {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      // leave only those params, that we know about
+      values = _.pick(values || {}, ForumUserGroupStore.keys);
+
+      forum.settings = forum.settings || {};
+
+      params.usergroup_ids.forEach(function (id) {
+        _.each(values, function (opts, key) {
+          forum.settings[key + ':usergroup:' + id] = {
+            value: opts.value,
+            force: !!opts.value
+          };
+        });
+      });
+
+      forum.markModified('settings');
+      forum.save(callback);
+    });
+  },
+  params: {
+    usergroup_ids: {
+      type: 'array',
+      required: true,
+      minItems: 1
+    },
+    forum_id: {
+      type: 'string',
+      required: true
+    }
+  }
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+module.exports = ForumUserGroupStore;
