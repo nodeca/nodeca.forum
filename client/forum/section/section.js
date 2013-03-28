aa215ff1b1ceaf311e3f0ceb98b8bc896ea4c698
Dervus Grim <dervus@lavabit.com>
2013-03-28 18:57:41 +0100

    Reorganize `section` and `thread` blocks.

--- client/forum/section/section.js
+++ client/forum/section/section.js
@@ -1,52 +1,48 @@
 'use strict';
 
 
-N.wire.on(module.apiPath, function (event) {
+N.wire.on('forum.section.append_next_page', function (event) {
   var $el     = $(event.currentTarget)
     , current = parseInt($el.data('current-page'), 10)
     , params  = {};
 
-  params.page     = current + 1;
-  params.id       = $el.data('thread-id');
-  params.forum_id = $el.data('forum-id');
+  params.page = current + 1;
+  params.id   = $el.data('forum-id');
 
-  N.io.rpc('forum.thread', params, function (err, payload) {
+  N.io.rpc('forum.section', params, function (err, payload) {
     if (err) {
       // common case:
-      // - amount of pages reduced (some posts were removed)
-      // - thread was moved or deleted
+      // - amount of pages reduced (some threads were moved or deleted)
       window.location = $el.attr('href');
       return;
     }
-
 
     // set current page to the one that was loaded
     $el.data('current-page', payload.data.page.current);
     payload.data.show_page_number = payload.data.page.current;
 
     // Update current state in the history
-    N.wire.emit('history_update', { payload: payload, url: $el.attr('href') });
+    N.wire.emit('history.update', { payload: payload, url: $el.attr('href') });
 
-    // Hide "More posts" button if there are no more pages.
+    // Hide "More threads" button if there are no more pages.
     // Or update the button's link to the next page.
     if (payload.data.page.current === payload.data.page.max) {
       $el.addClass('hidden');
     } else {
       $el.attr('href', N.runtime.router.linkTo(payload.data.head.apiPath, {
-        id:       payload.data.thread.id,
-        forum_id: payload.data.thread.forum_id,
-        page:     payload.data.page.current + 1
+        id:   payload.data.forum.id,
+        page: payload.data.page.current + 1
       }));
     }
 
-    var $html = $(N.runtime.render('forum.blocks.posts_list', payload.data));
-    $('.forum-post:last').after($html.hide());
+    var $html = $(N.runtime.render('forum.blocks.threads_list', payload.data));
+    $('.tl-thread-list:last').after($html.hide());
 
     // update pager
     $('.pagination').replaceWith(
       N.runtime.render('common.blocks.pagination', {
-        route:    'forum.thread',
-        params:   payload.data.thread,
+        route:    'forum.section',
+        params:   payload.data.forum,
         current:  payload.data.page.current,
         max_page: payload.data.page.max
       })
