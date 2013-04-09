e66524a05039e8c7e0983774eb52f87dab4b8b96
Dervus Grim <dervus@lavabit.com>
2013-04-09 17:35:51 +0200

    Update "More thread/posts" handlers for new `navigate.to` API.

--- client/forum/section/section.js
+++ client/forum/section/section.js
@@ -1,43 +1,27 @@
 'use strict';
 
 
-var _ = require('lodash');
-
-
 N.wire.on('forum.section.append_next_page', function (event) {
-  var $button = $(event.currentTarget)
-    , href    = $button.attr('href');
+  var $button = $(event.currentTarget);
 
-  N.wire.emit('navigate.to', { href: href, replaceState: true, skipRender: true }, function (err) {
-    if (err) {
-      // common case:
-      // - amount of pages reduced (some threads were moved or deleted)
-      window.location = href;
-      return;
-    }
+  function renderNextPage(data, callback) {
+    var $result, locals = data.locals;
 
-    // Get new history state directly from window.History because we cannot
-    // receive it from 'navigate.to' event handler - Wire does not provide
-    // such ability.
-    var state  = window.History.getState()
-      , data   = state.data
-      , locals = _.clone(data.locals);
-
-    locals.show_page_number = data.locals.page.current;
+    locals.show_page_number = locals.page.current;
 
     // Hide "More threads" button if there are no more pages.
     // Or update the button's link to the next page.
     if (locals.page.current === locals.page.max) {
       $button.addClass('hidden');
     } else {
-      $button.attr('href', N.runtime.router.linkTo(locals.head.apiPath, {
+      $button.attr('href', N.runtime.router.linkTo('forum.section', {
         id:   locals.forum.id,
         page: locals.page.current + 1
       }));
     }
 
-    var $html = $(N.runtime.render('forum.blocks.threads_list', locals));
-    $('.tl-thread-list:last').after($html.hide());
+    $result = $(N.runtime.render('forum.blocks.threads_list', locals)).hide();
+    $('.tl-thread-list:last').after($result);
 
     // update pager
     $('.pagination').replaceWith(
@@ -50,6 +34,13 @@ N.wire.on('forum.section.append_next_page', function (event) {
     );
 
     // show content
-    $html.fadeIn();
+    $result.fadeIn();
+    callback();
+  }
+
+  N.wire.emit('navigate.to', {
+    href:    $button.attr('href')
+  , render:  renderNextPage
+  , history: window.History.replaceState
   });
 });
--- client/forum/thread/thread.js
+++ client/forum/thread/thread.js
@@ -1,57 +1,47 @@
 'use strict';
 
 
-var _ = require('lodash');
-
-
 N.wire.on('forum.thread.append_next_page', function (event) {
-  var $button = $(event.currentTarget)
-    , href    = $button.attr('href');
+  var $button = $(event.currentTarget);
 
-  N.wire.emit('navigate.to', { href: href, replaceState: true, skipRender: true }, function (err) {
-    if (err) {
-      // common case:
-      // - amount of pages reduced (some posts were removed)
-      // - thread was moved or deleted
-      window.location = href;
-      return;
-    }
+  function renderNextPage(data, callback) {
+    var $result, locals = data.locals;
 
-    // Get new history state directly from window.History because we cannot
-    // receive it from 'navigate.to' event handler - Wire does not provide
-    // such ability.
-    var state  = window.History.getState()
-      , data   = state.data
-      , locals = _.clone(data.locals);
-
-    locals.show_page_number = data.locals.page.current;
+    locals.show_page_number = locals.page.current;
 
     // Hide "More posts" button if there are no more pages.
     // Or update the button's link to the next page.
-    if (data.locals.page.current === data.locals.page.max) {
+    if (locals.page.current === locals.page.max) {
       $button.addClass('hidden');
     } else {
-      $button.attr('href', N.runtime.router.linkTo(locals.head.apiPath, {
-        id:       locals.thread.id,
-        forum_id: locals.thread.forum_id,
-        page:     locals.page.current + 1
+      $button.attr('href', N.runtime.router.linkTo('forum.thread', {
+        id:       locals.thread.id
+      , forum_id: locals.thread.forum_id
+      , page:     locals.page.current + 1
       }));
     }
 
-    var $html = $(N.runtime.render('forum.blocks.posts_list', locals));
-    $('.forum-post:last').after($html.hide());
+    $result = $(N.runtime.render('forum.blocks.posts_list', locals)).hide();
+    $('.forum-post:last').after($result);
 
     // update pager
     $('.pagination').replaceWith(
       N.runtime.render('common.blocks.pagination', {
-        route:    'forum.thread',
-        params:   locals.thread,
-        current:  locals.page.current,
-        max_page: locals.page.max
+        route:    'forum.thread'
+      , params:   locals.thread
+      , current:  locals.page.current
+      , max_page: locals.page.max
       })
     );
 
     // show content
-    $html.fadeIn();
+    $result.fadeIn();
+    callback();
+  }
+
+  N.wire.emit('navigate.to', {
+    href:    $button.attr('href')
+  , render:  renderNextPage
+  , history: window.History.replaceState
   });
 });
