2f81c9390abe6178862ee212f53a005fd1574ce0
Dervus Grim <dervus@lavabit.com>
2013-07-03 22:56:56 +0200

    Drag only .section-control, not whole .section-group at section admin.

--- client/admin/forum/section/index.js
+++ client/admin/forum/section/index.js
@@ -6,48 +6,48 @@ var _ = require('lodash');
 
 N.wire.on('navigate.done:' + module.apiPath, function () {
   // Make sections draggable (both section control and children).
-  $('.section-group').draggable({
+  $('.section-control').draggable({
     handle: '.section-handle'
   , revert: 'invalid'
   , helper: 'clone'
   , opacity: 0.5
   , cursor: 'move'
   , start: function () {
-      var $this = $(this);
+      var $group = $(this).parent(); // Get whole .section-group
 
-      $this.addClass('section-dragging');
+      $group.addClass('section-dragging');
 
       // Calculate element offset relative to upper edge of viewport.
-      var screenOffsetTop = $this.offset().top - window.scrollY;
+      var screenOffsetTop = $(this).offset().top - window.scrollY;
   
       // Show all placeholders except useless (inner and surrounding).
       $('.section-placeholder')
-        .not($this.find('.section-placeholder'))
-        .not($this.prev('.section-placeholder'))
-        .not($this.next('.section-placeholder'))
+        .not($group.find('.section-placeholder'))
+        .not($group.prev('.section-placeholder'))
+        .not($group.next('.section-placeholder'))
         .show();
 
       // After placeholders are shown, restore the offset to prevent jerk effect.
-      window.scrollTo(window.scrollX, ($this.offset().top - screenOffsetTop));
+      window.scrollTo(window.scrollX, ($(this).offset().top - screenOffsetTop));
     }
   , stop: function () {
-      $(this).removeClass('section-dragging');
+      $(this).parent().removeClass('section-dragging');
       $('.section-placeholder').hide();
     }
   });
 
   // Make all placeholders (hidden by default) droppable.
   $('.section-placeholder').droppable({
-    accept: '.section-group'
+    accept: '.section-control'
   , hoverClass: 'section-placeholder-hover'
   , tolerance: 'pointer'
   , drop: function (event, ui) {
       // Move section and it's allied placeholder into new location.
-      ui.draggable.prev().filter('.section-placeholder').insertBefore(this);
-      ui.draggable.insertBefore(this);
+      ui.draggable.parent().prev().filter('.section-placeholder').insertBefore(this);
+      ui.draggable.parent().insertBefore(this);
 
       var self   = this
-        , _id    = ui.draggable.children('.section-control').data('id')
+        , _id    = ui.draggable.data('id')
         , parent = $(this).parents('.section-group:first').children('.section-control').data('id');
 
       N.io.rpc('admin.forum.section.update', { _id: _id, parent: parent }, function (err) {
