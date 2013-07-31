6d937e3c093c52d11f25d5d0123a141dcb81ac14
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-07-31 23:39:21 +0400

    added PINNED state to thread & comments

--- server/forum/thread/_statuses.js
+++ server/forum/thread/_statuses.js
@@ -1,13 +1,18 @@
 // Thread & post statuses
 
+// Thread statuses are optimized for paged fetches & indexes
+// Some statises can have extended info in additionsl field:
+//
+// - PINNED, HB - status_ext contains OPEN/CLOSED/PENDING state
+//
 exports.thread = {
   OPEN:         0,
   CLOSED:       1,
-  PENDING:      2,
-  DELETED:      3,
-  DELETED_HARD: 4,
-  OPEN_HB:      5, // hellbanned (open)
-  CLOSED_HB:    6, // hellbanned (closed)
+  PINNED:       2,
+  PENDING:      3,
+  DELETED:      4,
+  DELETED_HARD: 5,
+  HB:           6 // hellbanned
 };
 
 exports.post = {
