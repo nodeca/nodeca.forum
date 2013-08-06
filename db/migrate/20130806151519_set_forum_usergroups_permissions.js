4de9e22f5b904632cdc34d009824d08ccadfbbbe
Sergey Batishchev <sergej.batishchev@gmail.com>
2013-08-06 17:55:47 +0400

    Added permissions for administrators and members.

---
+++ db/migrate/20130806151519_set_forum_usergroups_permissions.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var _     = require('lodash');
+var async = require('async');
+var updateStoreSettings = require('nodeca.users/server/admin/users/usergroups/_lib/update_store_settings');
+
+module.exports.up = function (N, cb) {
+  var models = N.models;
+
+  async.series([
+    //add usergroup settings for admin
+    function (callback) {
+      models.users.UserGroup.findOne({ short_name: 'administrators' })
+
+        .exec(function (err, group) {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          group.raw_settings = _.extend({}, group.raw_settings, {
+            forum_can_reply: { value: true },
+            forum_can_start_topics: { value: true },
+            forum_mod_can_edit_titles: { value: true },
+            forum_mod_can_edit_posts: { value: true },
+            forum_mod_can_delete_threads: { value: true },
+            forum_mod_can_delete_posts: { value: true }
+          });
+          group.save(callback);
+        });
+    },
+
+    // add usergroup settings for member
+    function (callback) {
+      models.users.UserGroup.findOne({ short_name: 'members' })
+        .exec(function (err, group) {
+
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          group.raw_settings = _.extend({}, group.raw_settings, {
+            forum_can_reply: { value: true },
+            forum_can_start_topics: { value: true }
+          });
+          group.save(callback);
+        });
+    },
+
+    // Recalculate store settings of all groups.
+    function (callback) {
+      updateStoreSettings(N, callback);
+    }
+  ], cb);
+};
