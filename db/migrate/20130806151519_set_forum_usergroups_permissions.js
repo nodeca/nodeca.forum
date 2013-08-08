4d3370d93d9989187c4aa22630a20772d0a86ae4
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-08-08 18:51:13 +0400

    moderators visibility - on by default

--- db/migrate/20130806151519_set_forum_usergroups_permissions.js
+++ db/migrate/20130806151519_set_forum_usergroups_permissions.js
@@ -20,11 +20,9 @@ module.exports.up = function (N, cb) {
 
           group.raw_settings = _.extend({}, group.raw_settings, {
             forum_can_reply: { value: true },
-            forum_can_start_topics: { value: true }/*,
-            forum_mod_can_edit_titles: { value: true },
-            forum_mod_can_edit_posts: { value: true },
-            forum_mod_can_delete_threads: { value: true },
-            forum_mod_can_delete_posts: { value: true }*/
+            forum_can_start_topics: { value: true },
+
+            forum_mod_visible: { value: true }
           });
           group.save(callback);
         });
