8785cdd4d0c31b8081d544f05edda37287201c5a
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-08-18 20:55:18 +0400

    moved subsections fetch to `internal:forum.subsections_fill`

--- server/forum/index/index.js
+++ server/forum/index/index.js
@@ -3,21 +3,6 @@
 "use strict";
 
 
-var to_tree = require('../../../lib/to_tree.js');
-var subsectionsFilterVisible = require('../../../lib/subsections_filter_visible');
-
-
-var subsections_fields = [
-  '_id',
-  'hid',
-  'title',
-  'description',
-  'moderator_list',
-  'child_list',
-  'cache'
-];
-
-
 ////////////////////////////////////////////////////////////////////////////////
 
 module.exports = function (N, apiPath) {
@@ -28,85 +13,8 @@ module.exports = function (N, apiPath) {
   // Request handler
   //
   N.wire.on(apiPath, function (env, callback) {
-
-    env.extras.puncher.start('Get forums');
-
-    // build tree for 0..2 levels, starting from sections without parent
-    N.models.forum.Section
-        .find()
-        .where('level').lte(2)
-        .sort('display_order')
-        .setOptions({ lean: true })
-        .exec(function (err, subsections) {
-
-      env.extras.puncher.stop({ count: subsections.length });
-
-      if (err) {
-        callback(err);
-        return;
-      }
-
-      env.data.subsections = subsections;
-
-      subsectionsFilterVisible(N, env, callback);
-    });
+    N.wire.emit('internal:forum.subsections_fill', env, callback);
   });
-
-
-  //
-  // Build response:
-  //  - forums list -> filtered tree
-  //  - collect users ids (last posters & moderators)
-  //
-  N.wire.after(apiPath, function fill_forums_tree_and_users(env, callback) {
-
-    env.extras.puncher.start('Post-process forums/users');
-
-    if (env.session && env.session.hb) {
-      env.data.sections = env.data.sections.map(function (doc) {
-        doc.cache.real = doc.cache.hb;
-        return doc;
-      });
-    }
-
-
-    env.data.users = env.data.users || [];
-
-    // collect users from subsections
-    env.data.subsections.forEach(function (doc) {
-      // queue users only for first 2 levels (those are not displayed on level 3)
-      if (doc.level < 2) {
-        if (!!doc.moderator_list) {
-          doc.moderator_list.forEach(function (user) {
-            env.data.users.push(user);
-          });
-        }
-        if (doc.cache.real.last_user) {
-          env.data.users.push(doc.cache.real.last_user);
-        }
-      }
-    });
-
-    env.res.subsections = to_tree(env.data.subsections, null);
-
-    // Cleanup output tree - delete attributes, that are not white list.
-    // Since tree points to the same objects, that are in flat list,
-    // we use flat array for iteration.
-    env.data.subsections.forEach(function (doc) {
-      for (var attr in doc) {
-        if (doc.hasOwnProperty(attr) &&
-            subsections_fields.indexOf(attr) === -1) {
-          delete(doc[attr]);
-        }
-      }
-      delete (doc.cache.hb);
-    });
-
-    env.extras.puncher.stop();
-
-    callback();
-  });
-
 
   // Fill breadcrumbs info
   //
--- server/forum/section/section.js
+++ server/forum/section/section.js
@@ -6,29 +6,6 @@
 var _     = require('lodash');
 
 
-var to_tree = require('../../../lib/to_tree.js');
-var subsectionsFilterVisible = require('../../../lib/subsections_filter_visible');
-
-
-var subsections_out_fields = [
-  '_id',
-  'hid',
-  'title',
-  'description',
-  'moderator_list',
-  'child_list',
-  'cache'
-];
-
-
-var section_info_out_fields = [
-  'hid',
-  'title',
-  'description',
-  'is_category'
-];
-
-
 ////////////////////////////////////////////////////////////////////////////////
 
 module.exports = function (N, apiPath) {
@@ -47,10 +24,6 @@ module.exports = function (N, apiPath) {
   });
 
 
-  // shortcuts
-  var Section = N.models.forum.Section;
-
-
   // Just subcall forum.topic.list, that enchances `env`
   //
   N.wire.on(apiPath, function get_posts(env, callback) {
@@ -64,39 +37,17 @@ module.exports = function (N, apiPath) {
   });
 
 
-  // fetch visible sub-sections (only on first page)
+  // fetch visible sub-sections (only for the first page)
   //
   N.wire.after(apiPath, function fetch_visible_subsections(env, callback) {
 
-    env.data.subsections = [];
     // subsections fetched only on first page
     if (env.params.page > 1) {
       callback();
       return;
     }
 
-    env.extras.puncher.start('Get subsections');
-
-    var max_level = env.data.section.level + 2; // need two next levels
-
-    Section
-      .find({ parent_list: env.data.section._id })
-      .where('level').lte(max_level)
-      .sort('display_order')
-      .setOptions({ lean: true })
-      .exec(function (err, subsections) {
-
-      if (err) {
-        callback(err);
-        return;
-      }
-
-      env.extras.puncher.stop();
-
-      env.data.subsections = subsections;
-
-      subsectionsFilterVisible(N, env, callback);
-    });
+    N.wire.emit('internal:forum.subsections_fill', env, callback);
   });
 
 
@@ -106,58 +57,6 @@ module.exports = function (N, apiPath) {
   //  - topics
   //
   N.wire.after(apiPath, function fill_head_and_breadcrumbs(env, callback) {
-    var root, max_subsection_level;
-
-    env.extras.puncher.start('Post-process sections/topics/users');
-
-    //
-    // Process sections
-    //
-
-    if (env.session && env.session.hb) {
-      env.data.subsections = env.data.subsections.map(function (doc) {
-        doc.cache.real = doc.cache.hb;
-        return doc;
-      });
-    }
-
-
-    env.data.users = env.data.users || [];
-    max_subsection_level = env.data.section.level + 2;
-
-    // collect users from subsections
-    env.data.subsections.forEach(function (doc) {
-      // queue users only for first 2 levels (those are not displayed on level 3)
-      if (doc.level < max_subsection_level) {
-        if (!!doc.moderator_list) {
-          doc.moderator_list.forEach(function (user) {
-            env.data.users.push(user);
-          });
-        }
-        if (doc.cache.real.last_user) {
-          env.data.users.push(doc.cache.real.last_user);
-        }
-      }
-    });
-
-
-    root = env.data.section._id;
-    env.res.subsections = to_tree(env.data.subsections, root);
-
-    // Cleanup output tree - delete attributes, that are not white list.
-    // Since tree points to the same objects, that are in flat list,
-    // we use flat array for iteration.
-    env.data.subsections.forEach(function (doc) {
-      for (var attr in doc) {
-        if (doc.hasOwnProperty(attr) &&
-            subsections_out_fields.indexOf(attr) === -1) {
-          delete(doc[attr]);
-        }
-      }
-      delete (doc.cache.hb);
-    });
-
-
     //
     // Process topics
     //
@@ -177,6 +76,7 @@ module.exports = function (N, apiPath) {
 
     env.res.topics = env.data.topics;
 
+    env.data.users = env.data.users || [];
     // collect users from topics
     env.data.topics.forEach(function (doc) {
       if (doc.cache.real.first_user) {
@@ -213,7 +113,12 @@ module.exports = function (N, apiPath) {
     }
 
     // prepare section info
-    res.section  = _.pick(section, section_info_out_fields);
+    res.section  = _.pick(section, [
+      'hid',
+      'title',
+      'description',
+      'is_category'
+    ]);
   });
 
 
