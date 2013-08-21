57485e00d2b91c1dc1d6deccd525b21bad7c65e2
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-08-21 13:57:18 +0400

    Fixed back `more topics` button for sections

--- client/forum/section/section.js
+++ client/forum/section/section.js
@@ -1,46 +1,77 @@
 'use strict';
 
 
+////////////////////////////////////////////////////////////////////////////////
+// "More topics" button logic
+
 N.wire.on('forum.section.append_next_page', function (event) {
   var $button = $(event.currentTarget);
+  var new_url = $button.attr('href');
 
-  function renderNextPage(data, callback) {
-    var $result, locals = data.locals;
+  N.io.rpc(
+    'forum.section.list',
+    { hid: $button.data('section'), page: $button.data('page') },
+    function (err, res) {
 
-    locals.show_page_number = locals.page.current;
+      // Process errors
+      if (err) {
+        // Do redirect, if happened
+        if (err.code === N.io.REDIRECT) {
+          N.wire.emit('navigate.to', err.head.Location);
+          return;
+        }
+        // Notify user in other case
+        return false;
+      }
 
-    // Hide "More Topics" button if there are no more pages.
-    // Or update the button's link to the next page.
-    if (locals.page.current === locals.page.max) {
-      $button.addClass('hidden');
-    } else {
+      // if no topics - just disable 'More' button
+      if (!res.topics || !res.topics.length) {
+        N.wire.emit('notify', {
+          type: 'warning',
+          message: t('error_no_more_topics')
+        });
+        $button.addClass('hidden');
+        return;
+      }
+
+      res.show_page_number = res.page.current;
+
+      // render & inject topics list
+      var $result = $(N.runtime.render('forum.blocks.topics_list', res));
+      $('#topiclist > :last').after($result);
+
+      // update button data & state
+      $button.data('page', res.page.current + 1);
+
       $button.attr('href', N.runtime.router.linkTo('forum.section', {
-        hid:   locals.section.hid,
-        page: locals.page.current + 1
+        hid:          res.section.hid,
+        page:         res.page.current + 1
       }));
+
+      if (res.page.current === res.page.max) {
+        $button.addClass('hidden');
+      }
+
+      // update pager
+      $('._pagination').html(
+        N.runtime.render('common.blocks.pagination', {
+          route:    'forum.section'
+        , params:   { hid: res.section.hid }
+        , current:  res.page.current
+        , max_page: res.page.max
+        })
+      );
+
+      // update history / url / title
+      N.wire.emit('navigate.replace', {
+        href: new_url,
+        title: t('title_with_page', {
+          title: res.topic.title,
+          page: res.page.current
+        })
+      });
     }
+  );
 
-    $result = $(N.runtime.render('forum.blocks.topics_list', locals)).hide();
-    $('#topiclist > :last').after($result);
-
-    // update pager
-    $('._pagination').html(
-      N.runtime.render('common.blocks.pagination', {
-        route:    'forum.section',
-        params:   locals.section,
-        current:  locals.page.current,
-        max_page: locals.page.max
-      })
-    );
-
-    // show content
-    $result.fadeIn();
-    callback();
-  }
-
-  N.wire.emit('navigate.to', {
-    href: $button.attr('href')
-  , render: renderNextPage
-  , replaceState: true
-  });
+  return;
 });
