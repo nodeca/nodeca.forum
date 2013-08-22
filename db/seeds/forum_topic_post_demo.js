c8d46c0af8a03e6197be9e98ee70de82409d76a0
Sergey Batishchev <sergej.batishchev@gmail.com>
2013-08-22 13:49:05 +0400

    Improved forum seed

--- db/seeds/forum_topic_post_demo.js
+++ db/seeds/forum_topic_post_demo.js
@@ -34,147 +34,67 @@ var USER_COUNT = 200;
 var MAX_MODERATOR_COUNT = 3;
 var MAX_SUB_SECTION_COUNT = 3;
 
-var DISPLAY_ORDER_SHIFT = 2;
+var display_order = 0;
 
-// cache usergroups ids
-var usergroups_cache = {};
-
-// extend Charlatan
-// add numeric id generator
-Charlatan.Incrementer = {
-  display_order_shift: DISPLAY_ORDER_SHIFT
+var getNextDisplayOrder = function () {
+  display_order++;
+  return display_order;
 };
 
-Charlatan.Incrementer.next = function (type) {
-  var last_id_prop_name = type + '_last_id';
-  if (!this[last_id_prop_name]) {
-    var shift_prop_name = type + '_shift';
-    if (!this[shift_prop_name]) {
-      this[shift_prop_name] = 0;
-    }
-    this[last_id_prop_name] = this[shift_prop_name];
-  }
-  this[last_id_prop_name]++;
-  return this[last_id_prop_name];
-};
-
-Charlatan.users = [];
-
-// add helpers for categorys,sections, topics and posts
-Charlatan.Helpers.category = function () {
-  return {
-    title: Charlatan.Lorem.sentence(Charlatan.Helpers.rand(5, 3)).slice(0, -1),
-    description: Charlatan.Lorem.sentence(),
-
-    display_order: Charlatan.Incrementer.next('display_order'),
-    is_category: true
-  };
-};
-
-Charlatan.Helpers.section = function (parent) {
-  return {
-    title: Charlatan.Lorem.sentence(Charlatan.Helpers.rand(5, 3)).slice(0, -1),
-    description: Charlatan.Lorem.sentence(),
-
-    parent: parent._id,
-    display_order: Charlatan.Incrementer.next('display_order'),
-
-    cache: {
-      real: {}
-    }
-  };
-};
-
-Charlatan.Helpers.topic = function (section) {
-  return {
-    title: Charlatan.Lorem.sentence().slice(0, -1),
-
-    st: statuses.topic.OPEN,
-    section: section._id,
-
-    views_count: Charlatan.Helpers.rand(1000)
-  };
-};
+var users = [];
 
 var postDay = 0;
-Charlatan.Helpers.post = function (topic) {
-  var ts =  new Date(2010, 0, postDay++);
-  return {
+
+var createPost = function (topic, callback) {
+
+  var post = new Post({
     text: Charlatan.Lorem.paragraphs(Charlatan.Helpers.rand(5, 1)).join(' '),
-    fmt:  'txt',
+    fmt: 'txt',
 
     st: statuses.post.VISIBLE,
     topic: topic._id,
 
-    user: Charlatan.users[Charlatan.Helpers.rand(USER_COUNT)],
+    user: users[Charlatan.Helpers.rand(USER_COUNT)],
 
-    ts: ts
-    // ToDo user
-  };
-};
-
-
-Charlatan.Helpers.user = function () {
-  var nick = Charlatan.Internet.userName();
-  var first_name = Charlatan.Name.firstName();
-  var last_name = Charlatan.Name.lastName();
-  return {
-    first_name  : first_name,
-    last_name   : last_name,
-    nick        : nick,
-    name      : first_name + ' (' + nick + ') ' + last_name,
-
-    email       : Charlatan.Internet.email(),
-
-    joined_ts   : new Date()
-
-    // ToDo add groups
-  };
-};
-
-var is_big_topic = true;
-var is_big_section = true;
-
-
-var create_post = function (topic, callback) {
-  var post = new Post(Charlatan.Helpers.post(topic));
+    ts: new Date(2010, 0, postDay++)
+  });
 
   post.save(function (err, post) {
     if (err) {
       callback(err);
       return;
     }
-    var update = { $inc: { post_count: 1 }};
-    User.update({ _id: post.user }, update, function (err) {
+
+    User.update({ _id: post.user }, { $inc: { post_count: 1 }}, function (err) {
       callback(err, post);
     });
   });
 };
 
-var create_topic = function (section, callback) {
+var createTopic = function (section, post_count, callback) {
+
   var first_post;
   var last_post;
-  var post_count;
 
-  if (is_big_topic) {
-    is_big_topic = false;
-    post_count = POST_COUNT_IN_BIG_TOPIC;
-  } else {
-    post_count = 1;
-  }
+  var topic = new Topic({
+    title: Charlatan.Lorem.sentence().slice(0, -1),
 
-  var topic = new Topic(Charlatan.Helpers.topic(section));
+    st: statuses.topic.OPEN,
+    section: section._id,
 
-  async.series([
-    function (cb) {
-      topic.save(cb);
-    },
-    // create posts
-    function (cb) {
-      async.forEachSeries(_.range(post_count), function (current_post, next_post) {
-        create_post(topic, function (err, post) {
+    views_count: Charlatan.Helpers.rand(1000)
+  });
+
+  topic.save(function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    async.timesSeries(post_count, function (idx, next) {
+        createPost(topic, function (err, post) {
           if (err) {
-            next_post(err);
+            next(err);
             return;
           }
 
@@ -183,131 +103,122 @@ var create_topic = function (section, callback) {
           }
 
           last_post = post;
-          next_post();
+          next();
         });
-      }, cb);
-    },
-    // update topic
-    function (cb) {
-      topic.cache.real.post_count = post_count;
+      }, function (err) {
+        // update topic
+        if (err) {
+          callback(err);
+        }
 
-      topic.cache.real.first_post = first_post._id;
-      topic.cache.real.first_ts = first_post.ts;
-      topic.cache.real.first_user = first_post.user;
+        topic.cache.real.post_count = post_count;
 
-      topic.cache.real.last_post = last_post._id;
-      topic.cache.real.last_ts = last_post.ts;
-      topic.cache.real.last_user = last_post.user;
+        topic.cache.real.first_post = first_post._id;
+        topic.cache.real.first_ts = first_post.ts;
+        topic.cache.real.first_user = first_post.user;
 
-      _.extend(topic.cache.hb, topic.cache.real);
+        topic.cache.real.last_post = last_post._id;
+        topic.cache.real.last_ts = last_post.ts;
+        topic.cache.real.last_user = last_post.user;
 
-      topic.save(cb);
-    }
-  ], function (err) {
-    callback(err, topic);
+        _.extend(topic.cache.hb, topic.cache.real);
+
+        topic.save(callback);
+      }
+    );
   });
 };
 
-var create_section = function (category, sub_section_deep, callback) {
-  var last_topic;
-  var post_count = 0;
-  var topic_count;
+var createSection = function (category, sub_section_deep, callback) {
 
-  var sub_section_list = [];
-  var sub_section_hid_list = [];
+  var section = new Section({
+    title: Charlatan.Lorem.sentence(Charlatan.Helpers.rand(5, 3)).slice(0, -1),
+    description: Charlatan.Lorem.sentence(),
 
-  if (is_big_section) {
-    is_big_section = false;
-    topic_count = TOPIC_COUNT_IN_BIG_SECTION;
-  } else {
-    topic_count = 1;
-  }
+    parent: category._id,
+    display_order: getNextDisplayOrder(),
 
-  var section = new Section(Charlatan.Helpers.section(category));
+    cache: {
+      real: {}
+    }
+  });
 
-  async.series([
-    function (cb) {
-      section.save(cb);
-    },
-
-    // create topics
-    function (cb) {
-      async.forEachSeries(_.range(topic_count), function (current_topic, next_topic) {
-        create_topic(section, function (err, topic) {
-          if (err) {
-            next_topic(err);
-            return;
-          }
-          last_topic = topic;
-          post_count += topic.cache.real.post_count;
-          next_topic();
-        });
-      }, cb);
-    },
+  section.save(function (err) {
+    if (err) {
+      callback(err);
+      return;
+    }
 
     // add sub-sections
-    function (cb) {
-      if (!sub_section_deep || Charlatan.Helpers.rand(3) === 2) {
-        cb();
-        return;
-      }
-      var sub_section_count = Charlatan.Helpers.rand(MAX_SUB_SECTION_COUNT);
-      async.forEach(_.range(sub_section_count), function (current_section, next_section) {
-        create_section(section, sub_section_deep - 1, function (err, sub_section) {
-          sub_section_list.push(sub_section._id);
-          sub_section_hid_list.push(sub_section.hid);
-          next_section(err);
-        });
-      }, cb);
-    },
+    if (!sub_section_deep || Charlatan.Helpers.rand(3) === 2) {
+      callback();
+      return;
+    }
 
-    // update section dependent info
-    function (cb) {
-      section.cache.real.last_topic = last_topic._id;
-      section.cache.real.last_topic_hid = last_topic.hid;
-      section.cache.real.last_topic_title = last_topic.title;
-
-      var topic_real = last_topic.cache.real;
-      section.cache.real.last_post = topic_real.last_post;
-      section.cache.real.last_ts = topic_real.last_ts;
-      section.cache.real.last_user = topic_real.last_user;
-
-      section.cache.real.post_count = post_count;
-      section.cache.real.topic_count = topic_count;
-      _.extend(section.cache.hb, section.cache.real);
-
-      section.save(cb);
-    },
-
-    // add moderators
-    function (cb) {
-      var SectionModeratorStore = N.settings.getStore('section_moderator');
-
-      if (!SectionModeratorStore) {
-        cb('Settings store `section_moderator` is not registered.');
-        return;
-      }
-
-      async.timesSeries(Charlatan.Helpers.rand(MAX_MODERATOR_COUNT), function (index, next) {
-        var user = Charlatan.users[Charlatan.Helpers.rand(USER_COUNT)];
-
-        SectionModeratorStore.set(
-          { forum_mod_visible: { value: true } },
-          { section_id: section._id, user_id: user._id },
-          next
-        );
-      }, cb);
-    },
-  ], function (err) {
-    callback(err, section);
+    async.timesSeries(Charlatan.Helpers.rand(MAX_SUB_SECTION_COUNT), function (idx, next) {
+      createSection(section, sub_section_deep - 1, next);
+    }, callback);
   });
 };
 
 
-var create_categories = function (callback) {
-  var category;
-  async.forEachSeries(_.range(CATEGORY_COUNT), function (current_category, next_category) {
-    category = new Category(Charlatan.Helpers.category());
+var createUsers = function (callback) {
+
+  var userGroupsByName = {};
+
+  async.series([ function getUsersByName(cb) {
+      UserGroup.find().select('_id short_name').exec(function (err, groups) {
+        if (err) {
+          cb(err);
+          return;
+        }
+
+        // collect usergroups
+        groups.forEach(function (group) {
+          userGroupsByName[group.short_name] = group;
+        });
+        cb();
+      });
+
+    }, function (cb) {
+
+      async.timesSeries(USER_COUNT, function (current_user, next_user) {
+        var nick = Charlatan.Internet.userName();
+        var first_name = Charlatan.Name.firstName();
+        var last_name = Charlatan.Name.lastName();
+        var user = new User({
+          first_name: first_name,
+          last_name: last_name,
+          nick: nick,
+          name: first_name + ' (' + nick + ') ' + last_name,
+
+          email: Charlatan.Internet.email(),
+
+          joined_ts: new Date(),
+
+          usergroups: userGroupsByName['members']
+        });
+        user.save(next_user);
+
+        // add user to store
+        users.push(user);
+      }, cb);
+    }
+  ], function (err) {
+    callback(err);
+  });
+};
+
+var createSections = function (callback) {
+
+  async.timesSeries(CATEGORY_COUNT, function (current_category, next_category) {
+    var category = new Category({
+      title: Charlatan.Lorem.sentence(Charlatan.Helpers.rand(5, 3)).slice(0, -1),
+      description: Charlatan.Lorem.sentence(),
+
+      display_order: getNextDisplayOrder('display_order'),
+      is_category: true
+    });
 
     category.save(function (err) {
       if (err) {
@@ -316,49 +227,205 @@ var create_categories = function (callback) {
       }
 
       // create sections
-      async.forEachSeries(_.range(SECTION_COUNT), function (current_section, next_section) {
-        create_section(category, SUB_SECTION_DEEP, function (err/*, section */) {
-          next_section(err);
-        });
+      async.timesSeries(SECTION_COUNT, function (idx, next) {
+        createSection(category, SUB_SECTION_DEEP, next);
       }, next_category);
     });
-  }, function (err) {
+  }, callback);
+};
+
+function updateSectionStat(section, callback) {
+
+  var lastTopic;
+  var topicCount;
+  var postCount;
+
+  async.series([ function getLastTopic(cb) {
+      Topic.findOne({section: section._id}).select('_id hid')
+        .sort({ 'hid': 1 })
+        .exec(function (err, topic) {
+
+          if (err) {
+            cb(err);
+            return;
+          }
+
+          lastTopic = topic;
+
+          cb();
+        });
+    }, function getTopicCount(cb) {
+      Topic.count({section: section._id})
+        .exec(function (err, count) {
+
+          if (err) {
+            cb(err);
+            return;
+          }
+
+          topicCount = count;
+          cb();
+        });
+
+    }, function getPostCount(cb) {
+
+      Topic.aggregate(
+        { $match: { section: section._id }},
+        { $group: { _id: null, count: { $sum: '$cache.real.post_count' }}},
+        { $project: { _id: 0, count: 1 }}, function (err, sum) {
+
+          if (err) {
+            cb(err);
+            return;
+          }
+
+          postCount = (_.isArray(sum) && sum[0] && sum[0].count) ? sum[0].count : 0; // get first element of result
+          cb();
+        });
+    }
+  ], function (err) {
+
     if (err) {
       callback(err);
       return;
     }
-    // Added empty section to last category
-    var section = new Section(Charlatan.Helpers.section(category));
+
+    // No topic, just exit
+    if (!lastTopic) {
+      callback(err);
+      return;
+    }
+
+    section.cache.real.last_topic = lastTopic._id;
+    section.cache.real.last_topic_hid = lastTopic.hid;
+    section.cache.real.last_topic_title = lastTopic.title;
+
+    var topicReal = lastTopic.cache.real;
+    section.cache.real.last_post = topicReal.last_post;
+    section.cache.real.last_ts = topicReal.last_ts;
+    section.cache.real.last_user = topicReal.last_user;
+
+    section.cache.real.post_count = postCount;
+    section.cache.real.topic_count = topicCount;
+    _.extend(section.cache.hb, section.cache.real);
+
     section.save(callback);
   });
+}
+
+var createTopics = function (callback) {
+
+  Section.find({ is_category: false }).select('_id cache')
+    .sort({ 'hid': -1 })
+    .skip(1)
+    .exec(function (err, sections) {
+
+      if (err) {
+        callback();
+        return;
+      }
+
+      async.forEachSeries(sections, function (section, cb) {
+        //create topic with single post
+        createTopic(section, 1, function (err) {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          updateSectionStat(section, cb);
+        });
+      }, callback);
+    });
+};
+
+var fillBigSection = function (callback) {
+
+  Section.findOne({ is_category: false }).select('_id cache')
+    .sort({ 'hid': 1 })
+    .exec(function (err, section) {
+
+      if (err) {
+        callback();
+        return;
+      }
+
+      async.series([ function (cb) {
+        async.timesSeries(TOPIC_COUNT_IN_BIG_SECTION, function (idx, next) {
+            createTopic(section, 1, next);
+          }, cb);
+
+      }, function (cb) {
+          updateSectionStat(section, cb);
+        }
+      ], callback);
+    });
+};
+
+var addBigTopic = function (callback) {
+
+  Section.findOne({ is_category: false }).select('_id cache')
+    .sort({ 'hid': 1 })
+    .exec(function (err, section) {
+
+      if (err) {
+        callback();
+        return;
+      }
+
+      createTopic(section, POST_COUNT_IN_BIG_TOPIC, function (err) {
+        if (err) {
+          callback();
+          return;
+        }
+
+        updateSectionStat(section, callback);
+      });
+    });
+};
+
+var addModerators = function (callback) {
+
+  var SectionModeratorStore = N.settings.getStore('section_moderator');
+
+  if (!SectionModeratorStore) {
+    callback('Settings store `section_moderator` is not registered.');
+    return;
+  }
+
+  Section.find({ is_category: false }).select('_id')
+    .exec(function (err, sections) {
+
+      async.forEach(sections, function (section, cb) {
+
+        async.timesSeries(Charlatan.Helpers.rand(MAX_MODERATOR_COUNT), function (index, next) {
+          var user = users[Charlatan.Helpers.rand(USER_COUNT)];
+
+          SectionModeratorStore.set(
+            { forum_mod_visible: { value: true } },
+            { section_id: section._id, user_id: user._id },
+            next
+          );
+        }, cb);
+      }, callback);
+    });
 };
 
 module.exports = function (N, callback) {
-  Category  = N.models.forum.Section;
-  Section     = N.models.forum.Section;
-  Topic    = N.models.forum.Topic;
-  Post      = N.models.forum.Post;
-  User      = N.models.users.User;
+  Category = N.models.forum.Section;
+  Section = N.models.forum.Section;
+  Topic = N.models.forum.Topic;
+  Post = N.models.forum.Post;
+  User = N.models.users.User;
   UserGroup = N.models.users.UserGroup;
 
-  UserGroup.find().select('_id short_name').exec(function(err, groups) {
-    // collect usergroups
-    groups.forEach(function(group) {
-      usergroups_cache[group.short_name] = group;
-    });
+  async.series([
+    createUsers,
+    createSections,
+    createTopics,
+    fillBigSection,
+    addBigTopic,
+    addModerators
 
-    async.series([
-      function (next) {
-        async.forEachSeries(_.range(USER_COUNT), function (current_user, next_user) {
-          var user = new User(Charlatan.Helpers.user());
-          user.usergroups = usergroups_cache['members'];
-          user.save(next_user);
-
-          // add user to store
-          Charlatan.users.push(user);
-        }, next);
-      }
-    , create_categories
-    ], callback);
-  });
+  ], callback);
 };
