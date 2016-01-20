2fbc6ddfc4f42288c09c0d33bad06e725d2df259
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-01-21 02:19:42 +0300

    PostCountCache: rewrite to promises

--- models/forum/PostCountCache.js
+++ models/forum/PostCountCache.js
@@ -4,10 +4,9 @@
 
 
 var Mongoose = require('mongoose');
-var Schema = Mongoose.Schema;
+var Schema   = Mongoose.Schema;
 var _        = require('lodash');
-var async    = require('async');
-var thenify  = require('thenify');
+var co       = require('co');
 
 
 // Step between cached hids. Value should be big enough to:
@@ -15,7 +14,7 @@ var thenify  = require('thenify');
 // - improve count performance (between cached and required hids)
 // - avoid frequency cache hits misses
 //
-const CACHE_STEP_SIZE = 100;
+const CACHE_STEP_SIZE = 10;
 
 
 module.exports = function (N, collectionName) {
@@ -45,13 +44,13 @@ module.exports = function (N, collectionName) {
   // - hb (Boolean) - user hellbanned status
   // - callback (Function) - `function (err, cnt)`
   //
-  PostCountCache.statics.getCount = thenify.withCallback(function (src, version, hid, hb, callback) {
+  PostCountCache.statics.getCount = function (src, version, hid, hb) {
 
     // Get post count.
     //
     // We don't use `$in` because it is slow. Parallel requests with strict equality is faster.
     //
-    function countFn(hid, cut_from, callback) {
+    let countFn = co.wrap(function* (hid, cut_from) {
       var Post = N.models.forum.Post;
 
       // Posts with this statuses are counted on page (others are shown, but not counted)
@@ -62,112 +61,72 @@ module.exports = function (N, collectionName) {
         countable_statuses.push(Post.statuses.HB);
       }
 
-      var result = 0;
+      let counters = yield countable_statuses.map(
+        st => Post.find()
+                  .where('topic').equals(src)
+                  .where('st').equals(st)
+                  .where('hid').lt(hid)
+                  .where('hid').gte(cut_from)
+                  .count()
+      );
 
-      async.each(countable_statuses, function (st, next) {
-        Post.find()
-            .where('topic').equals(src)
-            .where('st').equals(st)
-            .where('hid').lt(hid)
-            .where('hid').gte(cut_from)
-            .count(function (err, cnt) {
-
-          if (err) {
-            next(err);
-            return;
-          }
-
-          result += cnt;
-          next();
-        });
-
-      }, function (err) {
-        callback(err, result);
-      });
-    }
+      return _.sum(counters);
+    });
 
     var cached_hid = hid - hid % CACHE_STEP_SIZE;
 
     // Use direct count for small numbers
     if (cached_hid === 0) {
-      countFn(hid, 0, callback);
-      return;
+      return countFn(hid, 0);
     }
 
     // Fetch cache record
-    N.models.forum.PostCountCache.findOne({ src: src })
-        .lean(true)
-        .exec(function (err, cache) {
+    return co(function* () {
+      let cache = yield N.models.forum.PostCountCache
+                            .findOne({ src: src })
+                            .lean(true);
 
-      if (err) {
-        callback(err);
-        return;
-      }
+      let path = [ 'data', (version || 0), (hb ? 'hb' : 'normal'), cached_hid ].join('.');
 
-      var path = [ 'data', (version || 0), (hb ? 'hb' : 'normal'), cached_hid ].join('.');
-
-      // If cache does not exists - use direct count and rebuild cache mark
+      // Has cache - use it
       if (_.has(cache, path)) {
 
         // If required hid equals to cached one - return cached value
         if (hid === cached_hid) {
-          callback(null, _.get(cache, path));
-          return;
+          return _.get(cache, path);
         }
 
         // Get count between cached hid and required one
-        countFn(hid, cached_hid, function (err, cnt) {
-          if (err) {
-            callback(err);
-            return;
-          }
+        let cnt = yield countFn(hid, cached_hid);
 
-          callback(null, cnt + _.get(cache, path));
-        });
-        return;
+        return cnt + _.get(cache, path);
       }
 
-      // If no cache
-      countFn(cached_hid, 0, function (err, cached_hid_value) {
-        if (err) {
-          callback(err);
-          return;
+      // If cache does not exists - use direct count and rebuild cache mark
+      let cached_hid_value = yield countFn(cached_hid, 0);
+
+      let update = { $set: { src: src } };
+
+      update.$set[path] = cached_hid_value;
+
+      // Remove all previous version keys if exists
+      Object.keys((cache || {}).data || {}).forEach(cache_version => {
+        if (cache_version < (version || 0)) {
+          update.$unset = update.$unset || {};
+          update.$unset['data.' + cache_version] = '';
         }
-
-        var update = { $set: { src: src } };
-
-        update.$set[path] = cached_hid_value;
-
-        // Remove all previous version keys if exists
-        Object.keys((cache || {}).data || {}).forEach(function (cache_version) {
-          if (cache_version < (version || 0)) {
-            update.$unset = update.$unset || {};
-            update.$unset['data.' + cache_version] = '';
-          }
-        });
-
-        // Update cache record
-        N.models.forum.PostCountCache.update({ src: src }, update, { upsert: true })
-            .exec(function (err) {
-
-          if (err) {
-            callback(err);
-            return;
-          }
-
-          // Get count between cached hid and required hid
-          countFn(hid, cached_hid, function (err, cnt) {
-            if (err) {
-              callback(err);
-              return;
-            }
-
-            callback(null, cnt + cached_hid_value);
-          });
-        });
       });
+
+      // Update cache record
+      yield N.models.forum.PostCountCache
+                .update({ src: src }, update, { upsert: true });
+
+      // Get count between cached hid and required hid
+      let cnt = yield countFn(hid, cached_hid);
+
+      return cnt + cached_hid_value;
     });
-  });
+  };
 
 
   N.wire.on('init:models', function emit_init_PostCountCache(__, callback) {
