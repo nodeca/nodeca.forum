e060e9a0b2092cd89597bc3d5cc79c6a046f00ff
Kirill Efimov <kirill89@gmail.com>
2016-03-21 19:05:54 +0300

    Infractions: fetch urls by more universal way

--- internal/forum/fill_content_urls.js
+++ internal/forum/fill_content_urls.js
@@ -1,4 +1,15 @@
-// Fill urls for infractions with type `FORUM_POST`
+// Fill urls for forum posts (`FORUM_POST`)
+//
+// In:
+//
+// - items ([Object])
+//   - id (ObjectId)
+//   - type (String)
+// - user_info
+//
+// Out:
+//
+// - urls (Object) - key is item _id, value is url
 //
 'use strict';
 
@@ -8,8 +19,8 @@ const _ = require('lodash');
 
 module.exports = function (N) {
 
-  N.wire.on('internal:users.infractions.fill_content_urls', function* infractions_fill_content_urls(data) {
-    let posts_ids = _.map(data.list.filter(i => i.src_type === 'FORUM_POST'), 'src_id');
+  N.wire.on('internal:common.fill_content_urls', function* infractions_fill_content_urls(data) {
+    let posts_ids = _.map(data.items.filter(i => i.type === 'FORUM_POST'), 'id');
 
     if (!posts_ids.length) return;
 
@@ -18,7 +29,7 @@ module.exports = function (N) {
     //
     let posts = yield N.models.forum.Post.find()
                           .where('_id').in(posts_ids)
-                          .select('_id hid topic')
+                          .select('_id hid topic st ste')
                           .lean(true);
 
 
@@ -26,13 +37,31 @@ module.exports = function (N) {
     //
     let topics = yield N.models.forum.Topic.find()
                           .where('_id').in(_.map(posts, 'topic'))
-                          .select('_id hid section')
+                          .select('_id hid section st ste')
                           .lean(true);
 
     topics = topics.reduce((acc, t) => {
       acc[t._id] = t;
       return acc;
     }, {});
+
+
+    // Check permissions to see posts
+    //
+    let restricted = [];
+
+    for (let i = 0; i < posts.length; i++) {
+      let access_env = { params: { topic: topics[posts[i].topic], posts: posts[i], user_info: data.user_info } };
+
+      // We should check permissions one by one because posts could be from different topics
+      yield N.wire.emit('internal:forum.access.post', access_env);
+
+      if (!access_env.data.access_read) {
+        restricted.push(posts[i]);
+      }
+    }
+
+    posts = _.difference(posts, restricted);
 
 
     // Fetch sections
@@ -47,6 +76,8 @@ module.exports = function (N) {
       return acc;
     }, {});
 
+
+    data.urls = data.urls || {};
 
     posts.forEach(post => {
       data.urls[post._id] = N.router.linkTo('forum.topic', {
