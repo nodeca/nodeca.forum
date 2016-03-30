726c97395beaeec6545ef051e0738dac62781744
Kirill Efimov <kirill89@gmail.com>
2016-03-30 18:23:15 +0300

    Infractions: fill_content_urls -> infractions_info

--- internal/users/infraction/info.js
+++ internal/users/infraction/info.js
@@ -1,15 +1,13 @@
-// Fill urls for forum posts (`FORUM_POST`)
+// Fill urls and titles for forum posts (`FORUM_POST`)
 //
 // In:
 //
-// - items ([Object])
-//   - id (ObjectId)
-//   - type (String)
-// - user_info
+// - infractions ([users.Infraction])
+// - user_info (Object)
 //
 // Out:
 //
-// - urls (Object) - key is item _id, value is url
+// - info (Object) - key is `src_id`, value { url, title }
 //
 'use strict';
 
@@ -17,10 +15,10 @@
 const _ = require('lodash');
 
 
-module.exports = function (N) {
+module.exports = function (N, apiPath) {
 
-  N.wire.on('internal:common.fill_content_urls', function* infractions_fill_content_urls(data) {
-    let posts_ids = _.map(data.items.filter(i => i.type === 'FORUM_POST'), 'id');
+  N.wire.on(apiPath, function* fetch_infraction_info(info_env) {
+    let posts_ids = _.map(info_env.infractions.filter(i => i.src_type === 'FORUM_POST'), 'src_id');
 
     if (!posts_ids.length) return;
 
@@ -37,7 +35,7 @@ module.exports = function (N) {
     //
     let topics = yield N.models.forum.Topic.find()
                           .where('_id').in(_.map(posts, 'topic'))
-                          .select('_id hid section st ste')
+                          .select('_id hid section st ste title')
                           .lean(true);
 
     topics = topics.reduce((acc, t) => {
@@ -51,7 +49,7 @@ module.exports = function (N) {
     let restricted = [];
 
     for (let i = 0; i < posts.length; i++) {
-      let access_env = { params: { topic: topics[posts[i].topic], posts: posts[i], user_info: data.user_info } };
+      let access_env = { params: { topic: topics[posts[i].topic], posts: posts[i], user_info: info_env.user_info } };
 
       // We should check permissions one by one because posts could be from different topics
       yield N.wire.emit('internal:forum.access.post', access_env);
@@ -76,15 +74,15 @@ module.exports = function (N) {
       return acc;
     }, {});
 
-
-    data.urls = data.urls || {};
-
     posts.forEach(post => {
-      data.urls[post._id] = N.router.linkTo('forum.topic', {
-        section_hid: sections[topics[post.topic].section].hid,
-        topic_hid: topics[post.topic].hid,
-        post_hid: post.hid
-      });
+      info_env.info[post._id] = {
+        title: topics[post.topic].title,
+        url: N.router.linkTo('forum.topic', {
+          section_hid: sections[topics[post.topic].section].hid,
+          topic_hid: topics[post.topic].hid,
+          post_hid: post.hid
+        })
+      };
     });
   });
 };
