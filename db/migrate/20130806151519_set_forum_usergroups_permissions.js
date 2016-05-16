3e4cddad63d6f5bc753ec09ecae736583c073eb7
Alex Kocharin <alex@kocharin.ru>
2016-05-16 15:58:12 +0000

    Remove force flag from banned usergroup

--- db/migrate/20130806151519_set_forum_usergroups_permissions.js
+++ db/migrate/20130806151519_set_forum_usergroups_permissions.js
@@ -14,6 +14,7 @@ module.exports.up = co.wrap(function* (N) {
   yield usergroupStore.set({
     forum_can_reply: { value: true },
     forum_can_start_topics: { value: true },
+    forum_can_close_topic: { value: true },
     forum_mod_can_pin_topic: { value: true },
     forum_mod_can_edit_posts: { value: true },
     forum_mod_can_delete_topics: { value: true },
@@ -34,6 +35,9 @@ module.exports.up = co.wrap(function* (N) {
   }, { usergroup_id: memberGroup._id });
 
   // add usergroup settings for violators
+  //
+  // note: it is a modifier group added to users in addition to their
+  //       existing usergroups, thus we should turn "force" flag on
 
   let violatorsGroup = yield N.models.users.UserGroup.findOne({ short_name: 'violators' });
 
@@ -49,10 +53,7 @@ module.exports.up = co.wrap(function* (N) {
   let bannedGroup = yield N.models.users.UserGroup.findOne({ short_name: 'banned' });
 
   yield usergroupStore.set({
-    forum_can_reply: { value: false, force: true },
-    forum_can_start_topics: { value: false, force: true },
-    forum_edit_max_time: { value: 0, force: true },
-    forum_can_close_topic: { value: false, force: true }
+    forum_edit_max_time: { value: 0 }
   }, { usergroup_id: bannedGroup._id });
 
   // Recalculate store settings of all groups.
