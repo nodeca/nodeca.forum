784ba5628c80b07bf62622bc4e1af3e8f05c9fc0
Kirill Efimov <kirill89@gmail.com>
2016-07-09 09:09:37 +0300

    fixed topic subscription fetch (user_id -> user)

--- server/forum/topic/topic.js
+++ server/forum/topic/topic.js
@@ -84,7 +84,7 @@ module.exports = function (N, apiPath) {
     }
 
     let subscription = yield N.models.users.Subscription
-                          .findOne({ user_id: env.user_info.user_id, to: env.data.topic._id })
+                          .findOne({ user: env.user_info.user_id, to: env.data.topic._id })
                           .lean(true);
 
     env.res.subscription = subscription ? subscription.type : null;
