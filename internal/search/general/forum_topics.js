045de50f5d854a86bb652787c7d601a276a82b33
Alex Kocharin <alex@kocharin.ru>
2017-01-20 17:21:38 +0000

    General search page stub

---
+++ internal/search/general/forum_topics.js
@@ -0,0 +1,51 @@
+// Execute search in forum topics
+//
+// In:
+//
+// - params.query
+// - params.sort
+// - params.days
+// - params.skip
+// - params.limit
+// - params.user_info
+//
+// Out:
+//
+// - count
+// - results
+// - users
+//
+
+'use strict';
+
+
+module.exports = function (N, apiPath) {
+
+  // Character list is taken from:
+  // http://sphinxsearch.com/forum/view.html?id=10003
+  function sphinx_escape(query) {
+    return query.replace(/[\\()|\-!@~"&/^$=]/g, '\\$1');
+  }
+
+  // Hook to register search type
+  //
+  N.wire.before('server:search.general.list', function register_forum_topic_search(env) {
+    env.data.content_types.push('forum_topics');
+  });
+
+
+  // Execute actual search
+  //
+  N.wire.on(apiPath, function* execute_search(locals) {
+    let results = yield N.search.execute([
+      [
+        // TODO: skip/limit, ordering, permissions
+        'SELECT objectid FROM forum_topics WHERE MATCH(?) LIMIT 0',
+        [ sphinx_escape(locals.params.query) ]
+      ],
+      "SHOW META LIKE 'total_found'"
+    ]);
+
+    locals.count = Number(results[1][0].Value);
+  });
+};
