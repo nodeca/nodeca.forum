62eb50ad994ae873b67ac5d7da24204042955ab9
Alex Kocharin <alex@kocharin.ru>
2017-01-25 12:20:53 +0000

    Implement topic and post search

--- internal/search/general/forum_posts.js
+++ internal/search/general/forum_posts.js
@@ -19,6 +19,12 @@
 'use strict';
 
 
+const _                = require('lodash');
+const sanitize_topic   = require('nodeca.forum/lib/sanitizers/topic');
+const sanitize_section = require('nodeca.forum/lib/sanitizers/section');
+const sanitize_post    = require('nodeca.forum/lib/sanitizers/post');
+
+
 module.exports = function (N, apiPath) {
 
   // Character list is taken from:
@@ -27,25 +33,86 @@ module.exports = function (N, apiPath) {
     return query.replace(/[\\()|\-!@~"&/^$=]/g, '\\$1');
   }
 
-  // Hook to register search type
-  //
-  N.wire.before('server:search.general.list', function register_forum_post_search(env) {
-    env.data.content_types.push('forum_posts');
-  });
-
 
   // Execute actual search
   //
   N.wire.on(apiPath, function* execute_search(locals) {
+    let query  = 'SELECT object_id FROM forum_posts WHERE MATCH(?) AND public=1';
+    let params = [ sphinx_escape(locals.params.query) ];
+
+    if (locals.params.period > 0) {
+      query += ' AND ts > ?';
+      // round timestamp to the lowest whole day
+      params.push(Math.floor(Date.now() / (24 * 60 * 60 * 1000) - locals.params.period) * 24 * 60 * 60);
+    }
+
+    // sort is either `date` or `rel`, sphinx searches by relevance by default
+    if (locals.params.sort === 'date') {
+      query += ' ORDER BY ts DESC';
+    }
+
+    query += ' LIMIT ?,?';
+    params.push(locals.params.skip);
+    params.push(locals.params.limit);
+
     let results = yield N.search.execute([
-      [
-        // TODO: skip/limit, ordering, permissions
-        'SELECT objectid FROM forum_posts WHERE MATCH(?) LIMIT 0',
-        [ sphinx_escape(locals.params.query) ]
-      ],
+      [ query, params ],
       "SHOW META LIKE 'total_found'"
     ]);
 
+    let posts = yield N.models.forum.Post.find()
+                          .where('_id').in(_.map(results[0], 'object_id'))
+                          .lean(true);
+
+    let topics = yield N.models.forum.Topic.find()
+                           .where('_id').in(_.map(posts, 'topic'))
+                           .lean(true);
+
+    let sections = yield N.models.forum.Section.find()
+                             .where('_id').in(_.map(topics, 'section'))
+                             .lean(true);
+
+    let posts_by_id    = _.keyBy(posts, '_id');
+    let topics_by_id   = _.keyBy(topics, '_id');
+
+    let posts_sanitized    = _.keyBy(yield sanitize_post(N, posts, locals.params.user_info), '_id');
+    let topics_sanitized   = _.keyBy(yield sanitize_topic(N, topics, locals.params.user_info), '_id');
+    let sections_sanitized = _.keyBy(yield sanitize_section(N, sections, locals.params.user_info), '_id');
+
+    let users = {};
+
+    locals.results = [];
+
+    for (let { object_id } of results[0]) {
+      let post = posts_by_id[object_id];
+      if (!post) continue;
+
+      let topic = topics_by_id[post.topic];
+      if (!topic) continue;
+
+      let access_env = { params: {
+        topic,
+        posts: post,
+        user_info: locals.params.user_info
+      } };
+
+      yield N.wire.emit('internal:forum.access.post', access_env);
+
+      if (!access_env.data.access_read) continue;
+
+      if (post.user) users[post.user] = true;
+      if (post.to_user) users[post.to_user] = true;
+      if (post.del_by) users[post.del_by] = true;
+      if (post.import_users) post.import_users.forEach(id => { users[id] = true; });
+
+      locals.results.push({
+        post:    posts_sanitized[post._id],
+        topic:   topics_sanitized[topic._id],
+        section: sections_sanitized[topic.section]
+      });
+    }
+
+    locals.users = Object.keys(users);
     locals.count = Number(results[1][0].Value);
   });
 };
--- internal/search/general/forum_topics.js
+++ internal/search/general/forum_topics.js
@@ -19,6 +19,11 @@
 'use strict';
 
 
+const _                = require('lodash');
+const sanitize_topic   = require('nodeca.forum/lib/sanitizers/topic');
+const sanitize_section = require('nodeca.forum/lib/sanitizers/section');
+
+
 module.exports = function (N, apiPath) {
 
   // Character list is taken from:
@@ -27,25 +32,78 @@ module.exports = function (N, apiPath) {
     return query.replace(/[\\()|\-!@~"&/^$=]/g, '\\$1');
   }
 
-  // Hook to register search type
-  //
-  N.wire.before('server:search.general.list', function register_forum_topic_search(env) {
-    env.data.content_types.push('forum_topics');
-  });
-
 
   // Execute actual search
   //
   N.wire.on(apiPath, function* execute_search(locals) {
+    let query  = 'SELECT object_id FROM forum_topics WHERE MATCH(?) AND public=1';
+    let params = [ sphinx_escape(locals.params.query) ];
+
+    if (locals.params.period > 0) {
+      query += ' AND ts > ?';
+      // round timestamp to the lowest whole day
+      params.push(Math.floor(Date.now() / (24 * 60 * 60 * 1000) - locals.params.period) * 24 * 60 * 60);
+    }
+
+    // sort is either `date` or `rel`, sphinx searches by relevance by default
+    if (locals.params.sort === 'date') {
+      query += ' ORDER BY ts DESC';
+    }
+
+    query += ' LIMIT ?,?';
+    params.push(locals.params.skip);
+    params.push(locals.params.limit);
+
     let results = yield N.search.execute([
-      [
-        // TODO: skip/limit, ordering, permissions
-        'SELECT objectid FROM forum_topics WHERE MATCH(?) LIMIT 0',
-        [ sphinx_escape(locals.params.query) ]
-      ],
+      [ query, params ],
       "SHOW META LIKE 'total_found'"
     ]);
 
+    let topics = yield N.models.forum.Topic.find()
+                           .where('_id').in(_.map(results[0], 'object_id'))
+                           .lean(true);
+
+    let sections = yield N.models.forum.Section.find()
+                             .where('_id').in(_.map(topics, 'section'))
+                             .lean(true);
+
+    let topics_by_id   = _.keyBy(topics, '_id');
+
+    let topics_sanitized   = _.keyBy(yield sanitize_topic(N, topics, locals.params.user_info), '_id');
+    let sections_sanitized = _.keyBy(yield sanitize_section(N, sections, locals.params.user_info), '_id');
+
+    let users = {};
+
+    let access_env = { params: { topics, user_info: locals.params.user_info } };
+
+    yield N.wire.emit('internal:forum.access.topic', access_env);
+
+    let is_topic_visible = {};
+
+    topics.forEach((topic, idx) => {
+      is_topic_visible[topic._id] = !!access_env.data.access_read[idx];
+    });
+
+    locals.results = [];
+
+    for (let { object_id } of results[0]) {
+      if (!is_topic_visible[object_id]) continue;
+
+      let topic = topics_by_id[object_id];
+      if (!topic) continue;
+
+      users[topic.cache.first_user] = true;
+      users[topic.cache.last_user] = true;
+
+      if (topic.del_by) users[topic.del_by] = true;
+
+      locals.results.push({
+        topic:   topics_sanitized[topic._id],
+        section: sections_sanitized[topic.section]
+      });
+    }
+
+    locals.users = Object.keys(users);
     locals.count = Number(results[1][0].Value);
   });
 };
