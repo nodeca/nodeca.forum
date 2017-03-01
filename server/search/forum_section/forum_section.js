f67eec65f7df1b62328ca76a838e3274740957b6
Alex Kocharin <alex@kocharin.ru>
2017-03-01 12:46:22 +0000

    Add a button to show area where search is performed

--- server/search/forum_section/forum_section.js
+++ server/search/forum_section/forum_section.js
@@ -25,30 +25,49 @@ module.exports = function (N, apiPath) {
     }
   });
 
+
+  // Fetch section
+  //
+  N.wire.before(apiPath, function* fetch_section(env) {
+    let section = yield N.models.forum.Section
+                            .findOne({ hid: Number(env.params.$query.hid) })
+                            .lean(true);
+
+    if (!section) throw N.io.NOT_FOUND;
+
+    env.data.section = section;
+  });
+
+
+  // Check if user can view this section
+  //
+  N.wire.before(apiPath, function* check_access(env) {
+    let access_env = { params: { sections: env.data.section, user_info: env.user_info } };
+
+    yield N.wire.emit('internal:forum.access.section', access_env);
+
+    if (!access_env.data.access_read) throw N.io.NOT_FOUND;
+  });
+
+
   N.wire.on(apiPath, function search_general(env) {
     let menu = _.get(N.config, 'search.forum_section.menu', {});
     let content_types = Object.keys(menu)
                          .sort((a, b) => (menu[a].priority || 100) - (menu[b].priority || 100));
-    let type = content_types[0];
+    let type = env.params.$query.type || content_types[0];
 
     env.res.head.title = env.t('title');
     env.res.head.robots = 'noindex,nofollow';
 
-    if (env.params.$query) {
-      let query = env.params.$query;
-
-      // validate content type
-      if (query.type && content_types.indexOf(query.type) === -1) {
-        throw N.io.BAD_REQUEST;
-      }
-
-      type = query.type;
-
-      env.res.query  = query.query;
-      env.res.sort   = query.sort;
-      env.res.period = query.period;
-      env.res.hid    = Number(query.hid);
+    // validate content type
+    if (env.params.$query.type && content_types.indexOf(env.params.$query.type) === -1) {
+      throw N.io.BAD_REQUEST;
     }
+
+    env.res.query  = env.params.$query.query;
+    env.res.sort   = env.params.$query.sort;
+    env.res.period = env.params.$query.period;
+    env.res.hid    = Number(env.params.$query.hid);
 
     env.res.type          = type;
     env.res.sort_types    = sort_types;
@@ -58,5 +77,7 @@ module.exports = function (N, apiPath) {
     // an amount of search results loaded at once,
     // it is expected to be overriden for different content types
     env.res.items_per_page = 40;
+
+    env.res.filter_title = env.data.section.title;
   });
 };
--- server/search/forum_topic/forum_topic.js
+++ server/search/forum_topic/forum_topic.js
@@ -22,18 +22,39 @@ module.exports = function (N, apiPath) {
     }
   });
 
+
+  // Fetch topic
+  //
+  N.wire.before(apiPath, function* fetch_topic(env) {
+    let topic = yield N.models.forum.Topic
+                          .findOne({ hid: Number(env.params.$query.hid) })
+                          .lean(true);
+
+    if (!topic) throw N.io.NOT_FOUND;
+
+    env.data.topic = topic;
+  });
+
+
+  // Check if user can view this topic
+  //
+  N.wire.before(apiPath, function* check_access(env) {
+    let access_env = { params: { topics: env.data.topic, user_info: env.user_info } };
+
+    yield N.wire.emit('internal:forum.access.topic', access_env);
+
+    if (!access_env.data.access_read) throw N.io.NOT_FOUND;
+  });
+
+
   N.wire.on(apiPath, function search_general(env) {
     env.res.head.title = env.t('title');
     env.res.head.robots = 'noindex,nofollow';
 
-    if (env.params.$query) {
-      let query = env.params.$query;
-
-      env.res.query  = query.query;
-      env.res.sort   = query.sort;
-      env.res.period = query.period;
-      env.res.hid    = Number(query.hid);
-    }
+    env.res.query  = env.params.$query.query;
+    env.res.sort   = env.params.$query.sort;
+    env.res.period = env.params.$query.period;
+    env.res.hid    = Number(env.params.$query.hid);
 
     // there are no tabs for search inside topic,
     // so only one content type possible
@@ -45,5 +66,7 @@ module.exports = function (N, apiPath) {
     // an amount of search results loaded at once,
     // it is expected to be overriden for different content types
     env.res.items_per_page = 40;
+
+    env.res.filter_title = env.data.topic.title;
   });
 };
