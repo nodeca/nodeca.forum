b742f775786ccd82e79849a0a209f052d0470245
Alex Kocharin <alex@kocharin.ru>
2017-10-03 13:32:03 +0000

    Make rebuilds bulk

--- internal/forum/post_rebuild.js
+++ internal/forum/post_rebuild.js
@@ -8,34 +8,51 @@ const _ = require('lodash');
 
 module.exports = function (N, apiPath) {
 
-  N.wire.on(apiPath, async function rebuild_post(post_id) {
-    let post = await N.models.forum.Post.findById(post_id);
+  N.wire.on(apiPath, async function rebuild_post(ids) {
+    if (!Array.isArray(ids)) ids = [ ids ];
 
-    if (!post) return;
+    let posts = await N.models.forum.Post.find()
+                          .where('_id').in(ids)
+                          .lean(true);
 
-    let params = await N.models.core.MessageParams.getParams(post.params_ref);
-    let result = await N.parser.md2html({
-      text:         post.md,
-      attachments:  post.attach,
-      options:      params,
-      imports:      post.imports,
-      import_users: post.import_users
-    });
+    let bulk = N.models.forum.Post.collection.initializeUnorderedBulkOp();
 
-    let updateData = {
-      tail: result.tail,
-      html: result.html
-    };
+    await Promise.all(posts.map(async post => {
+      let params = await N.models.core.MessageParams.getParams(post.params_ref);
+      let result = await N.parser.md2html({
+        text:         post.md,
+        attachments:  post.attach,
+        options:      params,
+        imports:      post.imports,
+        import_users: post.import_users
+      });
 
-    [ 'imports', 'import_users' ].forEach(field => {
-      if (!_.isEmpty(result[field])) {
-        updateData[field] = result[field];
-      } else {
-        updateData.$unset = updateData.$unset || {};
-        updateData.$unset[field] = true;
+      let updateData = {
+        $set: {
+          tail: result.tail,
+          html: result.html
+        }
+      };
+
+      let needsUpdate = !_.isEqual(result.tail, post.tail) ||
+                        !_.isEqual(result.html, post.html);
+
+      for (let field of [ 'imports', 'import_users' ]) {
+        if (!_.isEmpty(result[field])) {
+          updateData.$set[field] = result[field];
+          needsUpdate = needsUpdate || !_.isEqual(result[field].map(String), (post[field] || []).map(String));
+        } else {
+          updateData.$unset = updateData.$unset || {};
+          updateData.$unset[field] = true;
+          needsUpdate = needsUpdate || typeof post[field] !== 'undefined';
+        }
       }
-    });
 
-    await N.models.forum.Post.update({ _id: post._id }, updateData);
+      if (needsUpdate) {
+        bulk.find({ _id: post._id }).update(updateData);
+      }
+    }));
+
+    if (bulk.length > 0) await bulk.execute();
   });
 };
