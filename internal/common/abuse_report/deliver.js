a8ac871ea29d3c16bf738f5961a78b390972e761
Alex Kocharin <alex@kocharin.ru>
2021-03-24 19:38:57 +0300

    Use post.createWithTopic for creating abuse reports

--- internal/common/abuse_report/deliver.js
+++ internal/common/abuse_report/deliver.js
@@ -11,8 +11,9 @@
 'use strict';
 
 
-const _       = require('lodash');
-const render  = require('nodeca.core/lib/system/render/common');
+const _        = require('lodash');
+const render   = require('nodeca.core/lib/system/render/common');
+const userInfo = require('nodeca.users/lib/user_info');
 
 
 module.exports = function (N, apiPath) {
@@ -54,69 +55,11 @@ module.exports = function (N, apiPath) {
                         .where('hid').equals(N.config.bots.default_bot_hid)
                         .lean(true);
 
-    // Create new post
-    let post = new N.models.forum.Post();
-
-    post.html   = (await N.parser.md2html({ text: body, options })).html;
-    post.md     = body;
-    post.st     = N.models.forum.Post.statuses.VISIBLE;
-    post.user   = bot._id;
+    let topic = new N.models.forum.Topic({ title: subject });
+    let post = new N.models.forum.Post({ md: body, section: section_id });
     post.params = options;
 
-    let report_ref = await N.models.forum.AbuseReportRef.findOne()
-                              .where('src').equals(params.report.src)
-                              .lean(true);
-    let topic;
-
-    // If ref exists - try fetch topic
-    if (report_ref) {
-      topic = await N.models.forum.Topic.findOne()
-                        .where('_id').equals(report_ref.dest)
-                        .lean(true);
-
-      // If topic not exists - delete invalid ref
-      if (!topic) {
-        await N.models.forum.AbuseReportRef.deleteOne({ _id: report_ref._id });
-      }
-    }
-
-    // If topic does not created yet - create topic and ref
-    if (!topic) {
-      topic = new N.models.forum.Topic({
-        st: N.models.forum.Topic.statuses.OPEN,
-        section: section_id,
-        title: subject,
-        cache: { first_post: post._id, first_ts: post.ts, first_user: post.user },
-        cache_hb: { first_post: post._id, first_ts: post.ts, first_user: post.user }
-      });
-
-      report_ref = new N.models.forum.AbuseReportRef({
-        src: params.report.src,
-        dest: topic._id
-      });
-
-      await Promise.all([
-        topic.save(),
-        report_ref.save()
-      ]);
-
-      await N.models.forum.UserTopicCount.inc(bot._id, { section_id: topic.section, is_hb: false });
-    }
-
-    post.topic   = topic._id;
-    post.section = topic.section;
-
-    // We should save post before `updateCache` call because cache use topic data
-    await post.save();
-
-    await Promise.all([
-      N.models.forum.Topic.updateCache(topic._id),
-      N.models.forum.Section.updateCache(section._id)
-    ]);
-
-    await N.models.forum.UserPostCount.inc(bot._id, { section_id: topic.section, is_hb: false });
-
-    await N.queue.forum_topics_search_update_with_posts([ topic._id ]).postpone();
+    await post.createWithTopic(topic, await userInfo(N, bot._id));
 
     // Add report url to locals
     params.locals.report_topic_url = N.router.linkTo('forum.topic', { section_hid: section.hid, topic_hid: topic.hid });
