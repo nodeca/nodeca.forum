bb91d06e1b40c874b96598e8eade8d6424aeb597
Alex Kocharin <alex@kocharin.ru>
2022-03-25 15:10:40 +0300

    Remove filter button on "recent" page

--- client/forum/index/recent/recent.js
+++ client/forum/index/recent/recent.js
@@ -9,19 +9,4 @@ N.wire.once('navigate.done:' + module.apiPath, function page_once() {
     return N.io.rpc('forum.mark_read', { ts: N.runtime.page_data.mark_cut_ts })
                .then(() => N.wire.emit('navigate.reload'));
   });
-
-  // Exclude click
-  //
-  N.wire.on(module.apiPath + ':exclude', function exclude() {
-    let params = {};
-
-    return Promise.resolve()
-      .then(() => N.io.rpc('forum.index.exclude.sections', {}))
-      .then(res => {
-        Object.assign(params, res);
-        return N.wire.emit('forum.index.sections_exclude_dlg', params);
-      })
-      .then(() => N.io.rpc('forum.index.exclude.update', { sections_ids: params.selected }))
-      .then(() => N.wire.emit('navigate.reload'));
-  });
 });
